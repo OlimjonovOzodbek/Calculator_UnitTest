9bad855ce3928fca4c4dc5e772142d4ec865d582
Ozodbek <olimjanovozodbek32@gmail.com>
2024-03-13 17:50:09 +0500

    Add project files.

---
+++ Calculator.Test/Calc_Tests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace Calculator.Test
+{
+    public class Calc_Tests
+    {
+        private readonly Calc _calc = new Calc();
+        [Fact]
+
+        public void AddTest()
+        {
+      
+            var res = _calc.Add(1, 2);
+            Assert.Equal(3, res);
+        }
+        [Fact]
+        
+        public void DivideTest()
+        {
+            var res = _calc.Divide(9, 3);
+            Assert.Equal(3, res);
+        }
+        [Fact]
+
+        public void Subtract()
+        {
+            var res = _calc.Subtract(9, 3);
+            Assert.Equal(6, res);
+        }
+
+        [Fact]
+        public void Multiply()
+        {
+            var res = _calc.Multiply(9, 3);
+            Assert.Equal(27, res);
+        }
+
+    }
+}
